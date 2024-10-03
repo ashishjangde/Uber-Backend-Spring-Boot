cfc79e6efbb70132d1b519f52ff34f9bb155d4e0
Ashish Jangde <ashishjangde2003@gmail.com>
2024-10-03 19:04:07 +0530

    setting up the server

---
+++ src/main/java/com/example/uberbackendspringboot/UberBackendSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.uberbackendspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UberBackendSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UberBackendSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/uberbackendspringboot/UberBackendSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uberbackendspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UberBackendSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
